8fb383c7729f1b8500387eab5ada1584917403ed
Theo Hadj <theo.hadja@gmail.com>
2025-01-31 15:11:21 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,105 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+
+interface Pokemon {
+  id: number,
+  pokedexId: number,
+  name: string,
+  image: string,
+  sprite: string,
+  slug: string,
+  stats: {
+      HP: number,
+      attack: number,
+      defense: number,
+      special_attack: number,
+      special_defense: number,
+      speed: number
+  }
+}
+
+
+function CardPokemon({ pokemon, onClick }: { pokemon: Pokemon; onClick: () => void })
+ {
+  return (
+    <article onClick={onClick} className="cardPokemon">
+      <img src={pokemon.image} alt={pokemon.name} width={100} />
+      <h3>{pokemon.name}</h3>
+    </article>
+  );
+};
+
+
+
+function ProfilPokemon({ pokemon }: { pokemon: Pokemon }) {
+  return (
+    <article className="profilPokemon">
+      <h2>{pokemon.name}</h2>
+      <img src={pokemon.image} alt={pokemon.name} width={150} />
+      <h3>Stats:</h3>
+      <ul className="listStats">
+        <li>HP: {pokemon.stats?.HP}</li>
+        <li>Attack: {pokemon.stats?.attack}</li>
+        <li>Defense: {pokemon.stats?.defense}</li>
+        <li>Speed: {pokemon.stats?.speed}</li>
+      </ul>
+    </article>
+  );
+};
+
+
+
+
+function App() {
+
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+
+
+    
+
+
+  useEffect(() => {
+    
+    fetch("https://pokebuildapi.fr/api/v1/pokemon/limit/10")
+      .then((response) => response.json())
+      .then((data) => setPokemonList(data))
+      .catch((error) => console.error("Erreur lors du fetch :", error));
+  }, []);
+
+
+  useEffect(() => {
+
+    if (pokemon) {
+      document.title = pokemon?.name;
+    }
+
+
+    return () => {
+      document.title = "Pokedex";
+    };
+  }, [pokemon]);
+
+  console.log(pokemonList)
+
+
+
+
+  return (
+    <>
+      <div>
+        <h1>Pokedex</h1>
+        <div className="cardContainer">
+          {pokemonList.map((p) => (
+            <CardPokemon key={p.id} pokemon={p} onClick={() => setPokemon(p)} />
+          ))}
+        </div>
+        {pokemon && <ProfilPokemon pokemon={pokemon} />}
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/CardPokemon.tsx
@@ -0,0 +1,10 @@
+// import Pokemon from './App.tsx';
+
+// const CardPokemon = ({ pokemon, onClick }: { pokemon: Pokemon; onClick: () => void }) => {
+//     return (
+//       <article onClick={onClick} style={{ cursor: 'pointer', border: '1px solid black', padding: '10px', margin: '5px' }}>
+//         <img src={pokemon.image} alt={pokemon.name} width={100} />
+//         <h3>{pokemon.name}</h3>
+//       </article>
+//     );
+//   };
